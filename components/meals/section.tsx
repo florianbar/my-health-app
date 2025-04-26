69adf8dcf2ba5b6b31ed8f37cf5954251ae15928
Florian Bär <florianbar01@gmail.com>
2025-04-26 20:22:51 +0200

    add function to handle add  button on sections

--- components/meals/section.tsx
+++ components/meals/section.tsx
@@ -1,8 +1,11 @@
 import { View, Text, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
+import { TIMESLOTS } from "../../constants/timeslots";
 import { Meal } from "../../types/meals";
 import MealsItem from "./item";
 import TextButton from "../ui/buttons/text-button";
+import useMealsStore from "../../stores/meals";
 
 interface Props {
   title?: string;
@@ -10,11 +13,20 @@ interface Props {
 }
 
 function MealsSection({ title, meals }: Props) {
+  const navigation = useNavigation();
+
+  const { selectTime } = useMealsStore((state) => state.actions);
+
+  function handleAdd() {
+    selectTime(TIMESLOTS[title]);
+    navigation.navigate("add-meal", { name: "" });
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.title}>{title}</Text>
-        <TextButton iconName="add-outline" onPress={() => {}}>
+        <TextButton iconName="add-outline" onPress={handleAdd}>
           Add
         </TextButton>
       </View>
